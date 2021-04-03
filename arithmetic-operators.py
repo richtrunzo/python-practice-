963095af65c469f2da7597765485aced79d23a2c
Rtrunzo <richtrunzo@gmail.com>
2021-04-03 11:25:09 -0700

    practiced simple arithmetic in python

---
+++ arithmetic-operators.py
@@ -0,0 +1,51 @@
+# addition:
+
+print(1 + 10)
+
+print(10 + 40)
+
+#subtraction:
+
+print(10 - 2)
+
+print(11 - 5)
+
+
+#multiplication:
+
+print(5 * 5)
+
+print(10 * 11)
+
+
+#division:
+
+print(10 / 2)
+
+print(20 / 5)
+
+print(20 / 3)
+
+print(isinstance(5 / 3, float))
+
+# isinstance function returns True if the specified object is of the specified type, otherwise False.
+
+isinstance(5, int) # == true
+isinstance("t", str) # == true
+
+#modulus: this provides a remainder same as JS
+
+print(5 % 3) # == 2
+
+#exponents:
+
+print(5 ** 3) # == 125
+
+print(5 ** 1) # == 5
+
+#floor division: floor division // rounds the result down to the nearest whole number
+
+
+print(5 // 3) # == 1
+
+print(500 // 9) #== 55
