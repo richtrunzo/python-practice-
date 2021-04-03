c5201d7c0bddd421023dfa09d3f24261869a94c8
Rtrunzo <richtrunzo@gmail.com>
2021-04-03 13:38:31 -0700

    Practiced python operators

---
+++ assignment-operators.py
@@ -0,0 +1,35 @@
+# Assignment operator and bitwise operators:
+
+number = 5
+
+number += 3
+
+# number now equals 8
+
+number -= 2
+
+# number now equals 6
+
+number *= 3
+
+# number now equals 18
+
+number /= 2
+
+# number now equals 9
+
+###
+
+newNumber = 10
+
+newNumber %= 2
+
+# newNumber now equals 0
+
+###
+
+thirdNumber = 4
+
+thirdNumber **= 5
+
+# thirdNumber now equals 1024
