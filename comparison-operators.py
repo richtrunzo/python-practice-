1ebf1be39a4a1efff1453b3c5a02c1425c8c353a
Rtrunzo <richtrunzo@gmail.com>
2021-04-04 15:27:10 -0700

    Reviewed comparison operators

---
+++ comparison-operators.py
@@ -0,0 +1,19 @@
+
+    # Equal.
+    number = 5
+
+    # Not equal.
+    number != 3
+
+
+    # Greater than.
+    number > 3
+
+    # Less than.
+    number < 8
+
+    # Greater than or equal to
+    number >= 5
+
+    # Less than or equal to
+    number <= 5
