2894b7d553bb0659e9a8f8482bb58242cd567577
Rtrunzo <richtrunzo@gmail.com>
2021-04-09 14:04:50 -0600

    Practiced lists.

---
+++ data-types/lists.py
@@ -0,0 +1,11 @@
+ # Lists are very similar to arrays.
+
+ nums = [0, 1, 2, 3, 4, 5]
+
+ nums[0] # returns 0
+
+ nums[-1] #returns 5
+
+ nums[-3] # returns [3, 4, 5]
+
+ nums + [10, 11, 12]  # returns [0, 1, 2, 3, 4, 5, 10, 11, 12]
