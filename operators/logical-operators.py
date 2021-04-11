9af320ed375ba9887e49cffa1719aa0b68b40491
Rtrunzo <richtrunzo@gmail.com>
2021-04-11 11:08:36 -0700

    practiced logical operators

--- operators/logical-operators.py
+++ operators/logical-operators.py
@@ -9,3 +9,17 @@ first > 5 or second > 10
 first == second #false
 
 first != second #true
+
+3.) Write a single JSON structure that describes the contents of all shopping carts in a grocery store where every item in the store has a SKU.
+* The whole structure should include an array of "cart" objects
+* The cart object is defined as:
+   * A cart ID
+   * An array of items
+* An item is defined as:
+   * An item name
+   * An item SKU
+* Include a couple example carts with example items in them. You can make up SKUs, IDs and names.
+
+
+let allShoppingCar
+
